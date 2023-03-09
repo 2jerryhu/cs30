47f1b1be69ad25ce311ca9d2ba4b64621f984bdc
2jerryhu <jerryhu1995@gmail.com>
2023-03-09 12:25:09 -0600

    f

---
+++ Array Assignment/sketch.js
@@ -0,0 +1,17 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  circle(200, 200, 40);
+  
+}
--- index.md
+++ index.md
@@ -1,3 +1,6 @@
 ## Demos
 - [Bouncing Ball] (04-bouncing-balls)
 - [Cubic Disarray](05-cubic-disarray)
+
+## Projects
+- [Array and Object Notation](Array-Assignment)
