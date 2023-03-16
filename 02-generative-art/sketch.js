b4babc38a49a0c56fc838332d8b63ee82fb7085b
2jerryhu <jerryhu1995@gmail.com>
2023-03-16 15:12:59 -0600

    generative art demo

--- 02-generative-art/sketch.js
+++ 02-generative-art/sketch.js
@@ -1,15 +1,29 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
+// Generative Art
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(220);
+  lots_of_lines(20, 20);
 }
 
 function draw() {
-  background(220);
+  
+}
+
+function lots_of_lines(columns, rows) {
+  for (let x = 0; x < columns; x++){
+    for (let y = 0; y <rows; y++) {
+      let space_amount = width/columns;
+      diagonal_line(x * space_amount, y * space_amount, space_amount);
+    }
+  }
+}
+
+function diagonal_line(x, y, spacing) {
+  if (random(2) > 1){
+    line(x - spacing/2, y + spacing/2, x + spacing/2, y - spacing/2);
+  }
+  else{
+    line(x - spacing/2, y - spacing/2, x + spacing/2, y + spacing/2);
+  }
 }
