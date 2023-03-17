ba71d8536ace6054407693aaca0df914ee37c4fb
2jerryhu <jerryhu1995@gmail.com>
2023-03-17 14:31:01 -0600

    new thing

--- Array Assignment/sketch.js
+++ Array Assignment/sketch.js
@@ -1,18 +1,31 @@
-// Optical Illusion
-// bezier curves?
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
-
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  background("white");
+  lotsOfLines(50, 50);
 }
 
 function draw() {
-  background(0);
-  circle(200, 200, 40);
   
+}
+
+function lotsOfLines(cols, rows) {
+  for (let x = 0; x < cols; x++) {
+    for (let y = 0; y < rows; y++) {
+      let spaceAmount = width/cols;
+      diagonalLine(x*spaceAmount, y*spaceAmount, spaceAmount);
+    }
+  }
+}
+
+function diagonalLine(x, y, spacing) {
+  if (random(100) > 50) {
+    //positive slope
+    line(x - spacing/2, y + spacing/2,
+         x + spacing/2, y - spacing/2);
+  }
+  else {
+    //negative slope
+    line(x - spacing/2, y - spacing/2,
+         x + spacing/2, y + spacing/2);
+  }
 }
